8645e9dc3c97680847b7fc7fae98e758918a4cd1
nuncaesqueca <alanbruno@gmail.com>
2018-12-12 00:18:09 -0200

    Testing private fields

--- src/fetch.js
+++ src/fetch.js
@@ -4,7 +4,6 @@ import Tree from './tree';
 const listFunction = ({ moviesFolder }) => {
   const moviesTree = new Tree(moviesFolder);
   const moviesTreeContents = moviesTree.toList();
-  // const moviesTreeContents2 = moviesTree._listContents(moviesFolder, true)
   console.log(moviesTreeContents);
 };
 
