f9360adb8d6e7492b32935a4068735cb300d3439
nuncaesqueca <alanbruno@gmail.com>
2018-12-16 22:14:56 -0200

    Removing unused references

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import Process from 'process';
 import { Tree } from './classes';
 
 const listFunction = ({ path = 'D:\\Workspace\\subfetcher\\Filmes', extensions }: { path:string, extensions: string }): void => {
-  const ext: Array<string> = extensions ? extensions.trim().split(',') : [];
   const fileTree: Tree = new Tree(path, true, extensions);
   const folderContents: any = fileTree.all();
   console.log(folderContents);
